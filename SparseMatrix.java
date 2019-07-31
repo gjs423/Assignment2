23b82d9981662e572c635f625567cdcc23ae1862
gjs423 <52678780+gjs423@users.noreply.github.com>
2019-07-31 15:44:25 -0700

    Update SparseMatrix.java

--- SparseMatrix.java
+++ SparseMatrix.java
@@ -1,52 +1,70 @@
 public class SparseMatrix {
-    private int totalRows;
-    private int totalColumns;
-    private MatrixRow firstRow;
-    private MatrixColumn firstColumn;
-    private int row;
-    private int column;
-    private int value;
 
-    //Creating sparseMatrix With rows and columns
-    public SparseMatrix(int totalRows, int totalColumns) {
-        MatrixRow curRow = firstRow;
-        MatrixColumn curColumn = firstColumn;
-
-        for(int i = 0; i < totalRows; i++) {
-            for(int j = 0; j < totalColumns; j++) {
-                curColumn = curColumn.getNextColumn();
-            }
-            curRow = curRow.getNextRow();
-        }
-    }
-
-    //Inserting Values in the sparseMatrix
-    public void insertValue(int curRow, int curColumn, int val){
-    }
-
-    //Printing the SparseMatrix
-    public void printSparseMatrix() {
-        for(int i = 0; i < totalRows; i++) {
-            for(int j = 0; j < totalColumns; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
-    }
-
-    //Transpose
-
-
-    //Getters for row, column, value
-    public int getRow() {
-        return row;
-    }
-
-    public int getColumn() {
-        return column;
-    }
-
-    public int getValue() {
-        return value;
-    }
-}//end of class SparseMatrix
+	private int totalRow;
+	private int totalCol;
+	private MatrixRow firstRow;
+	private MatrixColumn firstCol;
+	
+	public SparseMatrix(int row, int col) {
+		
+		totalRow = row;
+		totalCol = col;
+		firstRow = new MatrixRow();
+		firstCol = new MatrixColumn();
+		
+		MatrixRow tempRow = firstRow;
+		MatrixColumn tempCol = firstCol;
+		for(int i = 0; i < row; i++) {
+			tempRow.setNextRow(new MatrixRow());
+			tempRow = tempRow.getNextRow();
+		}
+		for(int i = 0; i < col; i++) {
+			tempCol.setNextColumn(new MatrixColumn());
+			tempCol = tempCol.getNextColumn();
+n		}
+		
+	}
+	
+	public int getTotalRow() {
+		return totalRow;
+	}
+	public int getTotalCol() {
+		return totalCol;
+	}
+	public MatrixRow getFirstRow() {
+		return firstRow;
+	}
+	public MatrixColumn getFirstCol() {
+		return firstCol;
+	}
+	
+	//given the row, column, and a value node, inserts the node into that location
+	public void insertNode(int row, int col, ValueNode n) {
+		
+		MatrixRow tempRow = firstRow;
+		for(int i = 0; i < row; i++) {
+			tempRow = tempRow.getNextRow();
+		}
+		tempRow.insertBackValueNode(n);
+		insertNodeCol(n, col);
+		
+	}
+	
+	//used in insertNode to also insert into specific column
+	private void insertNodeCol(ValueNode n, int col) {
+		
+		MatrixColumn tempCol = firstCol;
+		for(int i = 1; i < col; i++) {
+			tempCol = tempCol.getNextColumn();
+		}
+		tempCol.insertBackValueNode(n);
+		
+	}
+	
+	public void print() {
+		
+		
+		
+	}
+	
+}
